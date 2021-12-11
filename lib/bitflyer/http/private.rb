c684a7a9e91546c165091ad2b4c5876fcb803043
Giao571 <67278257+Giao571@users.noreply.github.com>
2021-12-11 20:41:10 +0000

    user agent

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -77,4 +77,5 @@ module Bitflyer
 
         def cancel_child_order(product_code: 'BTC_JPY', child_order_id: nil, child_order_acceptance_id: nil)
           body = {
-            product_code: product_cod
+            product_code: product_code,
+   
