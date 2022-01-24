cdea43c188624818d82a28bd1b6b6e0234b43a59
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-01-24 09:08:09 +0000

    Char: ipmi_bt_sm, fix infinite loop

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -84,4 +84,4 @@ module Bitflyer
           @connection.post('/v1/me/cancelchildorder', body).body
         end
 
-        def send_parent_order(order_method: nil, minute_
+        def send_parent_order(order_method: nil, minute_to_exp
