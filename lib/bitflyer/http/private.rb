8f08ee5cb1e9422aaa88a84ef58f386792536429
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-05-24 08:42:29 +0000

    modify test

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -98,4 +98,4 @@ module Bitflyer
           body = {
             product_code: product_code,
             parent_order_id: parent_order_id,
-            parent_order_acceptance_id: parent_order_accep
+            parent_order_acceptance_id: parent_order_acceptance_id
