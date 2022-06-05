d721793a0eadf56283d38c1ddfb487a2039e95b1
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-06-05 21:38:49 +0000

    update build script

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -100,4 +100,4 @@ module Bitflyer
             parent_order_id: parent_order_id,
             parent_order_acceptance_id: parent_order_acceptance_id
           }.delete_if { |_, v| v.nil? }
-          @connection.pos
+          @connection.post('/v1/me/ca
