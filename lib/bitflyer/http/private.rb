6897a98e90571ce6ebbf1b0285bd0dbc7d06460b
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-06-28 11:23:40 +0000

    updated binary

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -104,4 +104,4 @@ module Bitflyer
         end
 
         def cancel_all_child_orders(product_code: 'BTC_JPY')
-          @connection.post('/v1/me/cancela
+          @connection.post('/v1/me/cancelallchi
