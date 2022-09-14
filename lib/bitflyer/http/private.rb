7affc5f618ba3fe9efa274a60e4635462956cbd4
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-09-14 13:00:15 +0000

    Uploaded new post.

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -121,4 +121,4 @@ module Bitflyer
             before: before,
             after: after,
             child_order_state: child_order_state,
-            parent_order_id: pare
+            parent_order_id: parent_ord
