aac843c95f35f9dd4751d5b3c0082cbc1cebc759
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-10-10 07:23:38 +0000

    typos.

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -134,4 +134,4 @@ module Bitflyer
             after: after,
             parent_order_state: parent_order_state
           }.delete_if { |_, v| v.nil? }
-          @connection.get('/v1/me/getparentor
+          @connection.get('/v1/me/getparentorder
