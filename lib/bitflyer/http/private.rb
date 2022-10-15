643846b36c9e8316f0a67bd9de9a95c2d6410432
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-10-15 10:53:55 +0000

    Postman updated apidocs/folder_files.apib

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -137,4 +137,4 @@ module Bitflyer
           @connection.get('/v1/me/getparentorders', query).body
         end
 
-        def parent_order(parent_order_id:
+        def parent_order(parent_order_id: nil,
