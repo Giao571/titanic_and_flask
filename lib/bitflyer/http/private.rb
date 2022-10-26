6a9913d52ee2de5f4383956660ca550a6ac456ec
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-10-26 12:09:08 +0000

    Trying stuff

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -141,4 +141,4 @@ module Bitflyer
           query = {
             parent_order_id: parent_order_id,
             parent_order_acceptance_id: parent_order_acceptance_id
-          }.delete_if 
+          }.delete_if { |_, v| v.n
