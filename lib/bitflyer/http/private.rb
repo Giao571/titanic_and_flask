fa48640f77bdee153703b727301748e56253cbdd
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-10-29 09:54:14 +0000

    sort

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -142,4 +142,4 @@ module Bitflyer
             parent_order_id: parent_order_id,
             parent_order_acceptance_id: parent_order_acceptance_id
           }.delete_if { |_, v| v.nil? }
-          @connection.get('/v1/me/getpa
+          @connection.get('/v1/me/getparent
