222ff26dd34873722fd12961d0aa1e4e0d411dd7
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-11-13 08:58:01 +0000

    Packaging.

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -151,4 +151,5 @@ module Bitflyer
           before: nil,
           after: nil,
           child_order_id: nil,
-          child_order_acceptance_id: n
+          child_order_acceptance_id: nil
+ 
