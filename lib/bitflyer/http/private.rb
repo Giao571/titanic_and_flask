904a865722850076f9502328170ac9c17f7d7ae2
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-12-01 15:25:40 +0000

    Sonar fixes

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -160,4 +160,4 @@ module Bitflyer
             after: after,
             child_order_id: child_order_id,
             child_order_acceptance_id: child_order_acceptance_id
-          }.delete_if { |_, v| v
+          }.delete_if { |_, v| v.nil? 
