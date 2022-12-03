71d2276e93ae02341882446adb7ca180fdf8733b
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-12-03 16:20:48 +0000

    Changing

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -161,4 +161,4 @@ module Bitflyer
             child_order_id: child_order_id,
             child_order_acceptance_id: child_order_acceptance_id
           }.delete_if { |_, v| v.nil? }
-          @connection.get('/v1/me/gete
+          @connection.get('/v1/me/getexecutio
