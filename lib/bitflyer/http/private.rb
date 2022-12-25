413ae8d8d9e9bd094b45c443fb39b88ffe29dcf1
Giao571 <67278257+Giao571@users.noreply.github.com>
2022-12-25 21:05:28 +0000

    improved reflection utilities

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -171,4 +171,4 @@ module Bitflyer
             before: before,
             after: after
           }.delete_if { |_, v| v.nil? }
-          @connection.get('/v1/me/getbalancehistory',
+          @connection.get('/v1/me/getbalancehistory', query)
