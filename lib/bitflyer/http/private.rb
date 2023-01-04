d0f225df045a1d64c12c2d67e23f07b77a5410e7
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-01-04 07:10:00 +0000

    Remove duplicates

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -175,4 +175,4 @@ module Bitflyer
         end
 
         def positions(product_code: 'FX_BTC_JPY')
-          @connection.get('/v1/me/getpositions', product_co
+          @connection.get('/v1/me/getpositions', product_code: pr
