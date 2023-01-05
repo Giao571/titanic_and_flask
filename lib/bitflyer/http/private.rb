bf30d5962de10b96c808c480e80d40af19eb3073
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-01-05 09:35:27 +0000

    A little code cleanup

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -175,4 +175,4 @@ module Bitflyer
         end
 
         def positions(product_code: 'FX_BTC_JPY')
-          @connection.get('/v1/me/getpositions', product_code: produc
+          @connection.get('/v1/me/getpositions', product_code: product_code
