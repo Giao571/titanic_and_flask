61f69827c5b68d2fdb607ae138e19dd75a88603a
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-01-15 20:10:09 +0000

    cpufreq: interactive: Modifying sync_freq implementation

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -179,4 +179,4 @@ module Bitflyer
         end
 
         def trading_commission(product_code: 'BTC_JPY')
-          @connection.get('v1/me/gettradingcommission', pr
+          @connection.get('v1/me/gettradingcommission', product_c
