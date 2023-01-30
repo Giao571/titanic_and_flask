a319f1cc318715b2eb3eaa66b61e2fde9ef579d6
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-01-30 19:14:11 +0000

    sync with Kovid's branch

--- lib/bitflyer/http.rb
+++ lib/bitflyer/http.rb
@@ -5,3 +5,15 @@ require 'bitflyer'
 require 'bitflyer/http/public'
 require 'bitflyer/http/private'
 require 'faraday'
+require 'faraday_middleware'
+require 'openssl'
+
+module Bitflyer
+  module HTTP
+    class Connection
+      extend Forwardable
+
+      def_delegators :@connection, :get, :post
+
+      def initialize(key, secret)
+        @connection = Faraday::Connection.new(url: 'https://api.bitflyer.jp') do |f|
