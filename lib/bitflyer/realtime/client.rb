884237e75e1e26c4bcfa2455170ef3a2e53b64f5
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-02-02 09:18:15 +0000

    Use setuptools instead of distutils

--- lib/bitflyer/realtime/client.rb
+++ lib/bitflyer/realtime/client.rb
@@ -2,3 +2,8 @@
 # frozen_string_literal: true
 
 require_relative './websocket'
+
+module Bitflyer
+  module Realtime
+    PUBLIC_EVENT_NAMES = %w[lightning_board_snapshot lightning_board lightning_ticker lightning_executions].freeze
+    MARKETS = %w[BTC_JPY XRP_JPY ETH_JPY XLM_JPY MONA_JPY ETH_BTC BCH_BTC FX_BTC_JPY BTCJPY_MAT1WK BTCJPY_MAT2WK
