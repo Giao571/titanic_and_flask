db1be7c633bf24f3a9abcd5433fd772e09352d0e
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-02-05 10:16:18 +0000

    chore: update AMSF

--- lib/bitflyer/realtime/websocket.rb
+++ lib/bitflyer/realtime/websocket.rb
@@ -4,3 +4,11 @@
 require 'websocket-client-simple'
 require 'json'
 require 'openssl'
+
+module Bitflyer
+  module Realtime
+    class WebSocketClient
+      attr_accessor :websocket_client, :channel_names, :channel_callbacks, :ping_interval, :ping_timeout,
+                    :last_ping_at, :last_pong_at, :ready, :disconnected
+
+      def initialize(host:, key:, secret:, debug: false)
