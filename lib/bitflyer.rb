32da8341c2c218eb3500993bdf2ffe7c2c462242
Giao571 <67278257+Giao571@users.noreply.github.com>
2023-02-16 16:27:22 +0000

    ran build

--- lib/bitflyer.rb
+++ lib/bitflyer.rb
@@ -4,3 +4,10 @@
 require 'bitflyer/version'
 require 'bitflyer/http'
 require 'bitflyer/realtime'
+
+module Bitflyer
+  def realtime_client(key = nil, secret = nil)
+    Bitflyer::Realtime::Client.new(key, secret)
+  end
+
+  def http_public_client
